8bfc14c314ed8b514f606f1208e9ad43a7749525
Ruvick <rudikov.web@ya.ru>
2021-06-30 01:29:19 +0300

    Скрипт корзины, отправка

--- header.php
+++ header.php
@@ -20,6 +20,14 @@
 
 </head>
 <body>
+<!-- Скрипт корзины, отправка -->
+<script>  
+    let main_page = "<?echo get_bloginfo("url"); ?>";
+    let kabinet_page = "<?echo get_the_permalink(93); ?>";
+    let bascet_page = "<?echo get_the_permalink(53); ?>";
+    let thencs_page = "<?echo get_the_permalink(56); ?>";
+    let nophoto_page = "<?echo get_bloginfo("template_url");?>/img/no-photo.jpg";
+</script> 
   <div class="wrapper">  
     <!-- Подключение  модальных окон-->
     <? include "modal-win.php";?>
