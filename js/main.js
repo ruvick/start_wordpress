931d131d23e8002c2321fdf75ac33a124cf1c96a
Ruvick <rudikov.web@ya.ru>
2021-08-05 12:11:18 +0300

    Добавил маску тел

--- js/main.js
+++ js/main.js
@@ -225,6 +225,11 @@ $ = jQuery;
 
 // Файлы jQuery---------------------------------------------------------------------------------------------------------------
 
+// Маска телефона
+var inputmask_phone = { "mask": "+9(999)999-99-99" };
+jQuery("input[type=tel]").inputmask(inputmask_phone);
+
+
 // Открытие модального окна
 $(".popup-quest").on('click', function (e) {
 	e.preventDefault();
